9acd6a0266fcf5fc958048637d2e132e86f5fd9d
stevenmcfarlane <stevemac72@gmail.com>
2015-07-31 16:14:06 -0700

    Webpack is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,70 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
-var concat = require('gulp-concat');
+// var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+var webpackConfig = {
+    entry: './www/js/app.js',
+    output: {
+      path: './www',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+        {test: /\.scss$/, loader: "style!css!sass"}
+      ]
+    }
+};
+
+gulp.task("webpack", function(callback) {
+    // run webpack
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
+
+gulp.task("webpack-dev-server", function(callback) {
+    // Start a webpack-dev-server
+    var compiler = webpack({
+        entry: './www/js/app.js',
+        output: {
+          path: '/www',
+          filename: 'bundle.js'
+        },
+        module: {
+          loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            {test: /\.scss$/, loader: "style!css!sass"}
+          ]
+        }
+    });
+
+    new WebpackDevServer(compiler, {
+        // server and middleware options
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        // Server listening
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+
+        // keep the server alive or continue?
+        callback();
+    });
+});
 
 gulp.task('default', ['sass']);
 
@@ -46,7 +101,6 @@ gulp.task('git-check', function(done) {
       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
     );
-    process.exit(1);
   }
   done();
 });
--- www/js/app.js
+++ www/js/app.js
@@ -3,34 +3,36 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'highcharts-ng'])
-.controller('AppCtrl', function($scope) {
-  $scope.chartConfig = {
-    options: {
-      chart: {
-          type: 'bar'
+require('../lib/highcharts-ng/dist/highcharts-ng');
+
+var app = angular.module('starter', ['ionic', 'highcharts-ng'])
+  .controller('AppCtrl', ($scope) => {
+    $scope.chartConfig = {
+      options: {
+        chart: {
+            type: 'bar'
+        }
+      },
+      series: [{
+          data: [10, 15, 12, 8, 7]
+      }],
+      title: {
+          text: 'Chart Title'
+      },
+
+      loading: false
+    };
+  })
+
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
-    },
-    series: [{
-        data: [10, 15, 12, 8, 7]
-    }],
-    title: {
-        text: 'Chart Title'
-    },
-
-    loading: false
-  }
-})
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   });
-})
